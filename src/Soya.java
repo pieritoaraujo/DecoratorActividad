4c2187c3e9d68d79b43bb766ec239387ce6f44d1
Piero Araujo <pieritoeeuu@gmail.com>
2026-06-21 21:17:07 -0500

    refactor: Adaptar complemento Soya para los precios

--- src/Soya.java
+++ src/Soya.java
@@ -1,12 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Soya extends DecoratorComplemento {
     Bebida bebida;
+    private final Map<Tamano, Double> precios = new HashMap<>();
+
     public Soya(Bebida bebida){
+
         this.bebida = bebida;
+        precios.put(Tamano.NORMAL, 0.15);
+        precios.put(Tamano.MEDIANO, 0.20);
+        precios.put(Tamano.GRANDE, 0.25);
     }
+    @Override
     public String getDescripcion(){
         return bebida.getDescripcion() + " con soya";
     }
+    @Override
     public double costo(){
         return .15 + bebida.costo();
+    }
+    @Override
+    public Tamano getTamano(){
+        return bebida.getTamano();
     }
 }

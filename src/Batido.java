8330a82a4ab230f60e55037a56b4d7425d1ae735
Piero Araujo <pieritoeeuu@gmail.com>
2026-06-21 20:58:04 -0500

    feat: Adaptar cafe batido para diferentes precios

--- src/Batido.java
+++ src/Batido.java
@@ -1,8 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Batido extends Bebida{
+
+    private final Map<Tamano, Double> precios = new HashMap<>();
+
     public Batido(){
         descripcion = "cafe batido";
+        precios.put(Tamano.NORMAL, 0.89);
+        precios.put(Tamano.MEDIANO, 0.99);
+        precios.put(Tamano.GRANDE, 1.09);
     }
+    @Override
     public double costo(){
-        return 0.89;
+        return precios.get(getTamano());
     }
 }

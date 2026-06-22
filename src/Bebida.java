35be8d3885f5926fcdfb4d16db322d1d08e6a5d8
Piero Araujo <pieritoeeuu@gmail.com>
2026-06-21 20:51:44 -0500

    feat: Agregar soporte para tamaños de bebidas

--- src/Bebida.java
+++ src/Bebida.java
@@ -1,6 +1,22 @@
 public abstract class Bebida {
     String descripcion = "Bebida desconocida";
 
+    public enum Tamano {
+        NORMAL,
+        MEDIANO,
+        GRANDE
+    }
+
+    private Tamano tamano = Tamano.NORMAL;
+
+    public Tamano getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(Tamano tamano) {
+        this.tamano = tamano;
+    }
+
     public String getDescripcion() {
         return descripcion;
     }

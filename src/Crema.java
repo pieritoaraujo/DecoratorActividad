fe22c80a33a9dc293def09437c6f58187f9a0211
Piero Araujo <pieritoeeuu@gmail.com>
2026-06-21 21:20:25 -0500

    refactor: Adaptar complemento Crema para los precios

--- src/Crema.java
+++ src/Crema.java
@@ -1,13 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Crema extends DecoratorComplemento {
     Bebida bebida;
+    private final Map<Tamano, Double> precios = new HashMap<>();
+
     public Crema(Bebida bebida){
         this.bebida = bebida;
+        precios.put(Tamano.NORMAL, 0.10);
+        precios.put(Tamano.MEDIANO, 0.15);
+        precios.put(Tamano.GRANDE, 0.20);
     }
-
+    @Override
     public String getDescripcion(){
         return bebida.getDescripcion() + " con crema";
     }
+    @Override
     public double costo(){
-        return .10 + bebida.costo();
+        return bebida.costo() + precios.get(bebida.getTamano());
+    }
+    @Override
+    public Tamano getTamano(){
+        return bebida.getTamano();
     }
 }

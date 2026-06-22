cda96717686fdc0616312de76239b94d7c2efb57
Piero Araujo <pieritoeeuu@gmail.com>
2026-06-21 20:55:49 -0500

    feat: Adaptar cafe tostado negro para diferentes precios

--- src/TostadoNegro.java
+++ src/TostadoNegro.java
@@ -1,8 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TostadoNegro extends Bebida{
+
+    private final Map <Tamano, Double> precios = new HashMap<>();
+
     public TostadoNegro(){
         descripcion = "cafe tostado negro";
+        precios.put(Tamano.NORMAL, 0.99);
+        precios.put(Tamano.MEDIANO, 1.09);
+        precios.put(Tamano.GRANDE, 1.19);
     }
+
+    @Override
     public double costo(){
-        return 0.99;
+        return precios.get(getTamano());
     }
 }

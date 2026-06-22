80b8bf0dd061e2b46d86d7decfe8df1dc1db9153
Piero Araujo <pieritoeeuu@gmail.com>
2026-06-21 21:12:23 -0500

    refactor: Adaptar complemento Moca para los precios

--- src/Moca.java
+++ src/Moca.java
@@ -1,12 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Moca extends DecoratorComplemento {
     Bebida bebida;
+
+    private final Map<Tamano, Double> precios = new HashMap<>();
+
     public Moca(Bebida bebida){
         this.bebida = bebida;
+        precios.put(Tamano.NORMAL, 0.10);
+        precios.put(Tamano.MEDIANO, 0.15);
+        precios.put(Tamano.GRANDE, 0.20);
     }
+    @Override
     public String getDescripcion(){
         return bebida.getDescripcion() + " con moca";
     }
+
+    @Override
     public double costo(){
-        return .20 + bebida.costo();
+        return bebida.costo() + precios.get(bebida.getTamano());
+    }
+    @Override
+    public Tamano getTamano(){
+        return bebida.getTamano();
     }
 }

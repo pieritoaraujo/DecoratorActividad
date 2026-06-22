e901caec983bfaf7006ee365c1545bb9e31ad7e5
Piero Araujo <pieritoeeuu@gmail.com>
2026-06-21 21:07:18 -0500

    refactor: Adaptar complemento Leche para los precios

--- src/Leche.java
+++ src/Leche.java
@@ -1,12 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Leche extends DecoratorComplemento {
     Bebida bebida;
+    private final Map<Tamano, Double> precios = new HashMap<>();
+
     public Leche (Bebida bebida){
         this.bebida = bebida;
+        precios.put(Tamano.NORMAL, 0.10);
+        precios.put(Tamano.MEDIANO, 0.15);
+        precios.put(Tamano.GRANDE, 0.20);
     }
+    @Override
     public String getDescripcion(){
         return bebida.getDescripcion() + " con leche";
     }
+    @Override
     public double costo(){
-        return .10 + bebida.costo();
+        return bebida.costo() + precios.get(bebida.getTamano());
+    }
+    @Override
+    public Tamano getTamano(){
+        return bebida.getTamano();
     }
 }

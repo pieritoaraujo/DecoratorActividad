fae86124faad5e3ce405a045281c560920c8ebf3
Piero Araujo <pieritoeeuu@gmail.com>
2026-06-21 21:01:16 -0500

    refactor: Adaptar cafe Descafeinado para diferentes precios

--- src/Descafeinado.java
+++ src/Descafeinado.java
@@ -1,8 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Descafeinado extends Bebida{
+
+    private final Map<Tamano, Double> precios = new HashMap<>();
+
     public Descafeinado(){
         descripcion = "cafe descafeinado";
+        precios.put(Tamano.NORMAL, 1.05);
+        precios.put(Tamano.MEDIANO, 1.15);
+        precios.put(Tamano.GRANDE, 1.25);
     }
     public double costo(){
-        return 1.05;
+        return precios.get(getTamano());
     }
 }
